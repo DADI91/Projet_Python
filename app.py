bd7d967afa47f3910a7fb6613b533d110b450b5f
DADI91 <48893991+DADI91@users.noreply.github.com>
2020-06-07 00:26:58 +0200

    Projet2

--- app.py
+++ app.py
@@ -1,66 +1,59 @@
-from PySide2 import QtWidgets
-import currency_converter
-
-class App(QtWidgets.QWidget) :
+from PySide2 import QtWidgets, QtCore
+from movie import get_movies
+from movie import Movie
+class App(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.c = currency_converter.CurrencyConverter()
-        self.setWindowTitle("Convertisser de devises")
+        self.setWindowTitle("Ciné club")
         self.setup_ui()
-        self.set_default_values()
-        self.setuo_connections()
-    
-
-    def setup_ui(self):
-        self.layout = QtWidgets.QHBoxLayout(self)
-        self.cbb_devisesFrom = QtWidgets.QComboBox()
-        self.spn_montant = QtWidgets.QSpinBox()
-        self.cbb_devisesTo = QtWidgets.QComboBox()
-        self.spn_montantConverti = QtWidgets.QSpinBox()
-        self.btn_inverser = QtWidgets.QPushButton("inverser devises")
-
-        self.layout.addWidget(self.cbb_devisesFrom)
-        self.layout.addWidget(self.spn_montant)
-        self.layout.addWidget(self.cbb_devisesTo)
-        self.layout.addWidget(self.spn_montantConverti)
-        self.layout.addWidget(self.btn_inverser)
-        
-        
-    def set_default_values(self):
-        self.cbb_devisesFrom.addItems(sorted(list(self.c.currencies)))
-        self.cbb_devisesTo.addItems(sorted(list(self.c.currencies)))
-        self.cbb_devisesFrom.setCurrentText("EUR")
-        self.cbb_devisesTo.setCurrentText("EUR")
-        
-        self.spn_montant.setRange(1, 1000000)
-        self.spn_montantConverti.setRange(1, 1000000)
-        
-        self.spn_montant.setValue(100 )
-        self.spn_montantConverti.setValue(100)
-        
-
-    def setuo_connections(self):
-        self.cbb_devisesFrom.activated.connect(self.compute)
-        self.cbb_devisesTo.activated.connect(self.compute)
-        self.spn_montant.valueChanged.connect(self.compute)
-        self.spn_montantConverti.valueChanged.connect(self.compute)
-        self.btn_inverser.clicked.connect(self.inverser_devises)
-    
-    def compute(self):
-        montant = self.spn_montant.value()
-        devises_From = self.cbb_devisesFrom.currentText()
-        devises_To = self.cbb_devisesTo.currentText()
-        try:
-            resultat = self.c.convert(montant, devises_From, devises_To)
-        except currency_converter.currency_converter.RateNotFoundError:
-            print("la convertion n'a pas fonctionné")
-        else:
-            self.spn_montantConverti.setValue(resultat)
+        self.populate_movies()
+        self.add_movies()
+        self.remove_movie()
+        self.setup_connection()
          
-    def inverser_devises(self): 
-        print("Inverser divises")
+    
+    def setup_ui(self):
+        self.main_layout = QtWidgets.QVBoxLayout(self)
 
+        self.le_movieTitle = QtWidgets.QLineEdit()
+        self.btn_addMovie = QtWidgets.QPushButton("Ajoutter un filme")
+        self.lw_movies = QtWidgets.QListWidget()
+        self.btn_removeMovies = QtWidgets.QPushButton("suprimer le(s) film(s)")
+
+        self.main_layout.addWidget(self.le_movieTitle)
+        self.main_layout.addWidget(self.btn_addMovie)
+        self.main_layout.addWidget(self.lw_movies)
+        self.main_layout.addWidget(self.btn_removeMovies)
+    
+    def setup_connection(self):
+        self.btn_addMovie.clicked.connect(self.add_movies)
+        self.btn_removeMovies.clicked.connect(self.remove_movie)
+
+
+    def populate_movies(self):
+        movies = get_movies()
+
+        for movies in movies:
+            lw_item = QtWidgets.QListWidgetItem(movie.title)
+            lw_item.setData(QtCore.Qt.UserRole, movie)
+            self.lw_movies.addItemes(lw_item)
+
+    def add_movies(self):
+        movie_title = self.le_movieTitle.text()
+        if not movie_title:
+            return False
+        
+        movie = Movie(title=movie_title)
+        resultat = movie.add_to_movies()
+        if resultat:
+            lw_item = QtWidgets.QListWidgetItem(movie.title)
+            lw_item.setData(QtCore.Qt.UserRole, movie)
+            self.lw_movies.addItems(lw_item)
+
+    def remove_movie(self):
+        print("on supprime un film")
+ 
 app = QtWidgets.QApplication([])
-win = App()
-win.show()  
-app.exec_() 
+win= App()
+win.show()
+app.exec_()
